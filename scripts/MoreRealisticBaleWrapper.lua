c41302d2578ab4206bd7538a36bb5516fc53f6ce
Jakob Tischler <jakobgithub@gmail.com>
2014-05-23 23:11:28 +0200

    Bale wrapper: fasten script, take "realFillType" into account #40

--- scripts/MoreRealisticBaleWrapper.lua
+++ scripts/MoreRealisticBaleWrapper.lua
@@ -1,13 +1,16 @@
---[[
-MoreRealisticBaleWrapper
-adds power consumption to bale wrappers
+-- MoreRealisticBaleWrapper
+-- adds power consumption and correct silage fill levels to the Ursus MR bale wrapper
 
-@author: Jakob Tischler
-@date: 20 May 2014
-@version: 0.2
-@history: 0.1 (20 May 2014) - power consumption
-          0.2 (23 May 2014) - adjust silage bale fillLevel depending on wrapped bale fillType
-]]
+-- @author: Jakob Tischler
+-- @date: 20 May 2014
+-- @version: 0.2
+-- @history: 0.1 (20 May 2014) - power consumption
+--           0.2 (23 May 2014) - adjust silage bale fillLevel depending on wrapped bale fillType
+--
+-- Copyright (C) 2014 Jakob Tischler
+
+
+if pdlc_ursusAddon == nil or pdlc_ursusAddon.BaleWrapper == nil or RealisticUtils == nil then return; end;
 
 MoreRealisticBaleWrapper = {};
 
@@ -15,31 +18,23 @@ function MoreRealisticBaleWrapper.prerequisitesPresent(specializations)
 	return SpecializationUtil.hasSpecialization(RealisticVehicle, specializations) and SpecializationUtil.hasSpecialization(Cylindered, specializations);
 end;
 
-local origGetBaleInRange;
 function MoreRealisticBaleWrapper:load(xmlFile)
+	-- power consumption
 	self.realWorkingPowerConsumption = getXMLFloat(xmlFile, 'vehicle.realWorkingPowerConsumption');
 	self.realCurrentPowerConsumption = 0;
 
-	for i, spec in pairs(self.specializations) do
-		if spec ~= MoreRealisticBaleWrapper and spec.getBaleInRange ~= nil then
-			-- print(('spec %d has function "getBaleInRange" -> set as origGetBaleInRange, overwrite with own'):format(i));
-			-- print(tableShow(spec.getBaleInRange, 'spec [before]'));
-			origGetBaleInRange = spec.getBaleInRange;
-			spec.getBaleInRange = MoreRealisticBaleWrapper.getBaleInRange;
-			-- print(tableShow(spec.getBaleInRange, 'spec [after]'));
-			break;
+	-- fillType to silage ratio
+	self.fillTypeRatio = {};
+	local exists, silageDensity = RealisticUtils.getFillTypeInfosV2('silage');
+	if not exists then return; end;
+
+	for fillType,_ in pairs(self.allowedBaleTypes) do
+		local fillTypeName = Fillable.fillTypeIntToName[fillType];
+		local exists, density = RealisticUtils.getFillTypeInfosV2(fillTypeName);
+		if exists then
+			self.fillTypeRatio[fillType] = density / silageDensity;
 		end;
 	end;
-
-	-- fillType to silage ratio
-	local _, grassWindrowDensity	 = RealisticUtils.getFillTypeInfosV2('grass_windrow');
-	local _, dryGrassWindrowDensity = RealisticUtils.getFillTypeInfosV2('dryGrass_windrow');
-	local _, silageDensity			 = RealisticUtils.getFillTypeInfosV2('silage');
-	self.fillTypeRatio = {
-		[ Fillable.fillTypeNameToInt.grass_windrow ]	= grassWindrowDensity / silageDensity;
-		[ Fillable.fillTypeNameToInt.dryGrass_windrow ]	= dryGrassWindrowDensity / silageDensity;
-	};
-	-- print(tableShow(self.fillTypeRatio, 'self.fillTypeRatio'));
 end;
 
 function MoreRealisticBaleWrapper:delete()
@@ -58,7 +53,7 @@ function MoreRealisticBaleWrapper:updateTick(dt)
 	if self.isServer and self.isActive and self.realWorkingPowerConsumption then
 		self.realCurrentPowerConsumption = 0;
 
-		if self.baleWrapperState == 3 then
+		if self.baleWrapperState == pdlc_ursusAddon.BaleWrapper.STATE_WRAPPER_WRAPPING_BALE then
 			self.realCurrentPowerConsumption = self.realWorkingPowerConsumption;
 			return;
 		end;
@@ -71,19 +66,27 @@ function MoreRealisticBaleWrapper:updateTick(dt)
 	end;
 end;
 
+local origGetBaleInRange = pdlc_ursusAddon.BaleWrapper.getBaleInRange;
 function MoreRealisticBaleWrapper.getBaleInRange(self, node)
 	local bale, silageBaleData = origGetBaleInRange(self, node);
-	if bale then
-		-- print(('getBaleInRange: bale=%s, silageBaleData=%s'):format(tostring(bale), tostring(silageBaleData)));
-		-- print(('\tfillType=%d (%s), fillLevel=%.2f'):format(bale.fillType, Fillable.fillTypeIntToName[bale.fillType], bale.fillLevel));
-		if silageBaleData and bale.fillLevel and bale.fillType then
-			bale.fillLevel = bale.fillLevel * (self.fillTypeRatio[bale.fillType] or 1);
-			-- print(('\tratio=%.5f -> set fillLevel to %.2f'):format(self.fillTypeRatio[bale.fillType], bale.fillLevel));
+	if bale and silageBaleData and bale.fillLevel and bale.fillType and (bale.isRealistic or bale.realSleepingMode1 ~= nil) then
+		local fillType = bale.fillType;
+		if bale.nodeId and bale.nodeId ~= 0 then
+			local realFillType = getUserAttribute(bale.nodeId, 'realFillType');
+			if realFillType then
+				fillType = Fillable.fillTypeNameToInt[realFillType];
+			end;
 		end;
+		-- print(('getBaleInRange: bale=%s, silageBaleData=%s'):format(tostring(bale), tostring(silageBaleData)));
+		-- print(('\tfillType=%d (%s), fillLevel=%.2f'):format(fillType, Fillable.fillTypeIntToName[fillType], bale.fillLevel));
+
+		bale.fillLevel = bale.fillLevel * (self.fillTypeRatio[fillType] or 1);
+		-- print(('\tratio=%.5f -> set fillLevel to %.2f'):format(self.fillTypeRatio[fillType], bale.fillLevel));
 	end;
 
 	return bale, silageBaleData;
 end;
+pdlc_ursusAddon.BaleWrapper.getBaleInRange = MoreRealisticBaleWrapper.getBaleInRange;
 
 function MoreRealisticBaleWrapper:draw()
 end;
