8f01e0b93645c095a86b3a62ac68e7d193402c76
Jakob Tischler <jakobgithub@gmail.com>
2014-07-23 23:06:34 +0200

    clean up RealisticMovingTool

--- specs/RealisticMovingTool.lua
+++ specs/RealisticMovingTool.lua
@@ -1,11 +1,12 @@
 -- RealisticMovingTool
 -- adds power consumption to implements with movingTools
 
--- @author: Jakob Tischler
--- @date: 17 May 2014
--- @version: 0.2
+-- @author: Jakob Tischler, dural
+-- @date: 20 Jul 2014
+-- @version: 0.3
 -- @history: 0.1 (Jan 2014)
 --           0.2 (17 May 2014)
+--           0.3 (20 Jul 2014): get moving parts via updateMovingPart() instead of looping through each movingTool [dural]
 --
 -- Copyright (C) 2014 Jakob Tischler
 
@@ -17,23 +18,13 @@ function RealisticMovingTool.prerequisitesPresent(specializations)
 end;
 
 function RealisticMovingTool:load(xmlFile)
-	self.realWorkingPowerConsumption = Utils.getNoNil(getXMLFloat(xmlFile, 'vehicle.realWorkingPowerConsumption'), 0);
+	self.realWorkingPowerConsumption = getXMLFloat(xmlFile, 'vehicle.realWorkingPowerConsumption');
 	self.realCurrentPowerConsumption = 0;
 	self.powerConsumptionRequired = false;
 end;
 
-function RealisticMovingTool:delete()
-end;
-
-function RealisticMovingTool:mouseEvent(posX, posY, isDown, isUp, button)
-end;
-
-function RealisticMovingTool:keyEvent(unicode, sym, modifier, isDown)
-end;
-
-local abs = math.abs;
 function RealisticMovingTool:updateTick(dt)
-	if self.isServer and self.isActive and self.realWorkingPowerConsumption > 0 then
+	if self.isServer and self.isActive and self.realWorkingPowerConsumption then
 		self.realCurrentPowerConsumption = 0;
 
 		if self.powerConsumptionRequired then
@@ -43,16 +34,16 @@ function RealisticMovingTool:updateTick(dt)
 	end;
 end;
 
-function RealisticMovingTool:update(dt)
-end;
-
-function RealisticMovingTool:draw()
-end;
-
 function RealisticMovingTool.setPowerConsumptionRequired(self, part, isInitialUpdate)
-	if self.isServer and part.playSound then
+	if self.isServer and self.isRealistic and part.playSound then
 		self.powerConsumptionRequired = true;
 	end;
 end;
 Cylindered.updateMovingPart = Utils.appendedFunction(Cylindered.updateMovingPart, RealisticMovingTool.setPowerConsumptionRequired);
+
+function RealisticMovingTool:delete() end;
+function RealisticMovingTool:mouseEvent(posX, posY, isDown, isUp, button) end;
+function RealisticMovingTool:keyEvent(unicode, sym, modifier, isDown) end;
+function RealisticMovingTool:update(dt) end;
+function RealisticMovingTool:draw() end;
 
